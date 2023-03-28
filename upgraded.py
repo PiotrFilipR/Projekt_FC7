1df287cee363737d2e417a9a51e335005cc3fabf
RieurSanglier <rytel.filip@gmail.com>
2023-03-28 21:02:12 +0200

    Do konsultacji

--- upgraded.py
+++ upgraded.py
@@ -2,56 +2,54 @@ import requests
 import datetime
 import os
 
+url = "https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=" \
+      "rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}"
+longitude = 20.490189
+latitude = 53.770226
+
 
 class WeatherForecast:
 
-    def __init__(self):
+    def __init__(self, searched_date):
         self.opady = {}
+        self.searched_date = searched_date
+
         with open("opady.txt", 'r') as f:
             odczyt = f.readlines()
             for record in odczyt:
-                self.opady[record.split()[1].replace("\n", "")] = record.split()[0]
+                self.opady[record.split()[0]] = record.split()[1].replace("\n", "")
+
+        for line in odczyt: ###### Nie czyta tego!
+            if line.split()[0] == str(self.searched_date):
+                suma_opadow = float(line.split()[1])
+        else:
+            resp = requests.get(url.format(latitude=latitude, longitude=longitude, searched_date=searched_date)). \
+                json()
+            suma_opadow = resp['daily']['rain_sum']
+            with open("opady.txt", 'a') as f:
+                f.write(str(self.searched_date) + " " + str(suma_opadow[0]) + "\n")
+
+        self.opady[self.searched_date] = suma_opadow
+
 
     def __repr__(self):
-        return f'<Oto zapisane opady: {self.opady}>'
+        return f'<Oto wszystkie znane daty, wraz z prognozą: {self.opady}>'
+
+    def __setitem__(self, searched_date, opad):
+        self.opady[searched_date] = opad
 
     def __getitem__(self, searched_date):
+        return self.opady[searched_date]
 
-        suma_opadow = None
-
-        url = "https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=" \
-              "rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}"
-        longitude = 20.490189
-        latitude = 53.770226
-
-        with open("opady.txt", 'r') as f:
-            odczyt = f.readlines()
-            for line in odczyt:
-                if line.split()[0] == str(searched_date):
-                    if float(line.split()[1]) == 0:
-                        print("Nie będzie padać.")
-                    elif float(line.split()[1]) > 0:
-                        print("Będzie padać.")
-                else:
-                    resp = requests.get(url.format(latitude=latitude, longitude=longitude, searched_date=searched_date)).json()
-                    suma_opadow = resp['daily']['rain_sum']
-                    if suma_opadow[0] == 0:
-                        print("Nie będzie padać.")
-                    elif suma_opadow[0] > 0:
-                        print("Będzie padać.")
-                    else:
-                        print("Nie wiem.")
-                    break
-
-        if suma_opadow:
-            with open("opady.txt", 'a') as f:
-                f.write(str(searched_date) + " " + str(suma_opadow[0]) + "\n")
-
-    def items(self):
+    def __dict__(self):
         return self.opady
+    def items(self):
+        for d, o in self.opady.items():
+            yield d, o
 
     def __iter__(self):
         return iter(self.opady)
+
 
 if not os.path.exists('opady.txt'):
     with open('opady.txt', 'w'):
@@ -69,89 +67,24 @@ if data:
 else:
     searched_date = datetime.date.today() + datetime.timedelta(days=1)
 
+
+weather_forecast = WeatherForecast(searched_date)
 print(f"Wybrana data: {searched_date}")
 
-prognoza = WeatherForecast()
+print(weather_forecast.__dict__())
+print(f'W dniu {searched_date}, suma opadów będzie wynosiła: ', weather_forecast[searched_date], '[mm]')
+print()
+print(weather_forecast.items())
+print()
+print(weather_forecast)
 
-print(prognoza.items())
-
-# prognoza_dzis = prognoza[searched_date]
-
-# for record in prognoza():
+# print("\nDrukowanie dat:")
+#
+# for record in prognoza:
 #     print(record)
+#
+# print("\nDrukowanie kluczy i wartości:")
+# for a, b in prognoza.items():
+#     print(a, b)
 
 
-
-
-###########################################
-
-#
-# import requests
-# import datetime
-# import os
-#
-#
-# class WeatherForecast:
-#
-#     def __init__(self):
-#         self.opady = {}
-#
-#     def __getitem__(self, searched_date):
-#
-#         suma_opadow = None
-#
-#         url = "https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=" \
-#               "rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}"
-#         longitude = 20.490189
-#         latitude = 53.770226
-#
-#         with open("opady.txt", 'r') as f:
-#             odczyt = f.readlines()
-#             for line in odczyt:
-#                 if line.split()[0] == str(searched_date):
-#                     if float(line.split()[1]) == 0:
-#                         print("Nie będzie padać.")
-#                     elif float(line.split()[1]) > 0:
-#                         print("Będzie padać.")
-#                 else:
-#                     resp = requests.get(url.format(latitude=latitude, longitude=longitude, searched_date=searched_date)).json()
-#                     suma_opadow = resp['daily']['rain_sum']
-#                     if suma_opadow[0] == 0:
-#                         print("Nie będzie padać.")
-#                     elif suma_opadow[0] > 0:
-#                         print("Będzie padać.")
-#                     else:
-#                         print("Nie wiem.")
-#                     break
-#
-#         if suma_opadow:
-#             with open("opady.txt", 'a') as f:
-#                 f.write(str(searched_date) + " " + str(suma_opadow[0]) + "\n")
-#
-#     def items(self):
-#         return self.opady
-#
-#     def __iter__(self):
-#         return iter(self.data)
-#
-# if not os.path.exists('opady.txt'):
-#     with open('opady.txt', 'w'):
-#         pass
-#
-# data = input("Aby sprawdzić opady deszczu w Olsztynie:\n"
-#              "Podaj datę lub naciśnij enter (Wymagany format YYYY-MM-DD): ")
-#
-# if data:
-#     try:
-#         searched_date = datetime.datetime.strptime(data, "%Y-%m-%d").date()
-#     except:
-#         print("Podano złą datę.")
-#         searched_date = datetime.date.today() + datetime.timedelta(days=1)
-# else:
-#     searched_date = datetime.date.today() + datetime.timedelta(days=1)
-#
-# print(f"Wybrana data: {searched_date}")
-#
-# prognoza = WeatherForecast()
-#
-# prognoza_dzis = prognoza[searched_date]
